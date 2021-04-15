4e742c672618fb93dfa69917b4203fc76857aef1
wipro-vivek <vivek.rajendran2@wipro.com>
2021-04-15 19:02:39 +0530

    sme-response-changes-15/4

--- src/app/shared/fileUpload/fileUpload.component.ts
+++ src/app/shared/fileUpload/fileUpload.component.ts
@@ -8,11 +8,39 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class FileUploadComponent implements OnInit {
     @Input() questionDatas
     @Output("formChange") change: EventEmitter<any> = new EventEmitter();
+    fileData: any = [];
+    fileNames=[]
+    baseFileData
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.fileNames=this.questionDatas.response ? this.questionDatas.response : []
+     }
 
     onChange(event){
-        this.change.emit(event)
-    }    
+        let baseData=[]
+        for(let i = 0; i < event.target.files.length; i++) {
+            var reader = new FileReader();
+            reader.readAsDataURL(<File>event.target.files[i]);
+            reader.onload = function () {
+                baseData.push(reader.result)
+            };
+            this.fileNames.push(<File>event.target.files[i].name)
+        }
+
+        this.baseFileData= baseData
+            let obj={
+                questionDatas: this.questionDatas,
+                value:this.fileNames,
+                number:this.questionDatas.number
+            }
+        this.change.emit(obj)
+        event.target.value=''
+    }  
+
+    onFileRemove(index){
+       this.fileNames.splice(index,1)
+    }
+
 }
