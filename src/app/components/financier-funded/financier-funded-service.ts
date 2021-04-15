2ad735a6a554c0ad0609fabb71f149ecfd3fe2f5
Wipro-jegadeesh <jegadeesh.baskaran@wipro.com>
2021-04-15 18:27:40 +0530

    Funded search API

--- src/app/components/financier-funded/financier-funded-service.ts
+++ src/app/components/financier-funded/financier-funded-service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
 import { ApiService } from "../../service/api.service"
 import { environment } from '../../../environments/environment';
+import * as moment from 'moment';
+
 @Injectable()
 export class FinancierFundedServices {
   public baseUrl: string;
@@ -17,7 +19,15 @@ export class FinancierFundedServices {
     return this.apiService.tempGet(environment.serviePath_2+'api/v1/financing-details/getFinancingDetails/'+id);
   }
   searchFinanceFunded(params){
-    return this.apiService.tempGet(environment.serviePath_2+'api/v1/financing-details/searchFinanceFunded?finId'+localStorage.getItem("userId")+'&invNo='+params.invoiceRef+'&smeId='+params.smeId+'&buyerName='+params.buyerName+'&invoiceDate=2021-04-01&invDueDate=2021-04-17');
+    console.log("params.invoiceDate",params.invoiceDate);
+    let invoiceRef  = params.invoiceRef == undefined ? "" : params.invoiceRef;
+    let smeId  = params.smeId == undefined ? "" : params.smeId;
+    let buyerName  = params.buyerName == undefined ? "" : params.buyerName;
+    let invoiceDate  = params.invoiceDate == undefined ? "" : moment(params.invoiceDate).format('YYYY-MM-DD');
+    let invDueDate  = params.invDueDate == undefined ? "" :  moment(params.invDueDate).format('YYYY-MM-DD');
+    
+    http://localhost:8081/api/v1/financing-details/searchFinanceFunded?finId=FIN001&invNo=1234&smeId=SME101&buyerName=steel&invoiceDate=2021-04-01&invDueDate=2021-04-17
+    return this.apiService.tempGet(environment.serviePath_2+'api/v1/financing-details/searchFinanceFunded?finId='+localStorage.getItem("userId")+'&invNo='+invoiceRef+'&smeId='+smeId+'&buyerName='+buyerName+'&invoiceDate='+invoiceDate+'&invDueDate='+invDueDate);
 
   }
 }
