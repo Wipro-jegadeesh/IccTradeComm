f1386a4ce21ddbf5226ab21af88d3e86ef041c78
Wipro-jegadeesh <jegadeesh.baskaran@wipro.com>
2021-05-03 18:14:05 +0530

    Sectors

---
+++ src/app/shared/constants/signUpSectors.ts
@@ -0,0 +1,80 @@
+export const SIGNUPSECTORS=[
+    {
+        "id": "1",
+        "itemName": "Manufacturing"
+    },
+    {
+        "id": "1",
+        "itemName": "Automobiles and Components"
+    },
+    {
+        "id": "1",
+        "itemName": "Capital Goods"
+    },
+    {
+        "id": "1",
+        "itemName": "Commercial and Professional Services"
+    },
+    {
+        "id": "1",
+        "itemName": "Consumer Durables and Apparel"
+    },
+    {
+        "id": "1",
+        "itemName": "Energy"
+    },
+    {
+        "id": "1",
+        "itemName": "Food & Beverage"
+    },
+    {
+        "id": "1",
+        "itemName": "Food and Staples Retailing"
+    },
+    {
+        "id": "1",
+        "itemName": "Health Care Equipment and Services"
+    },
+    {
+        "id": "1",
+        "itemName": "Household and Personal Products"
+    },
+    {
+        "id": "1",
+        "itemName": "Media and Entertainment"
+    },
+    {
+        "id": "1",
+        "itemName": "Pharmaceuticals, Biotechnology, and Life Sciences"
+    },
+    {
+        "id": "1",
+        "itemName": "Construction Equipment"
+    },
+    {
+        "id": "1",
+        "itemName": "Semiconductors and Semiconductor Equipment"
+    },
+    {
+        "id": "1",
+        "itemName": "Software and Services"
+    },
+    {
+        "id": "1",
+        "itemName": "Technology Hardware and Equipment"
+    },
+    {
+        "id": "1",
+        "itemName": "Telecommunication Services"
+    },
+    {
+        "id": "1",
+        "itemName": "Transportation"
+    },
+    {
+        "id": "1",
+        "itemName": "Others"
+    },
+]
+
+   
