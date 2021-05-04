61544688012b06ceaa101163e85f337f2848d499
Wipro-jegadeesh <jegadeesh.baskaran@wipro.com>
2021-05-04 16:59:50 +0530

    Icc-sme-list

---
+++ src/app/components/icc-list-smes/icc-list-smes.service.ts
@@ -0,0 +1,13 @@
+
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs';
+import { ApiService } from "../../service/api.service"
+import { environment } from '../../../environments/environment';
+@Injectable()
+export class IccListSmeServices {
+  public baseUrl: string;
+  constructor(private apiService: ApiService) { }
+  getallSmeProfileDetails(id){
+    return this.apiService.tempGet(environment.serviePath_1+'sme-userprofile/allSmeProfileDetails'+id); 
+  }
+}
