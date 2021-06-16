1fce1c684f8cea3317de3433c842d5d0cf1d1a10
wipro-nasar <syed.ibrahim5@wipro.com>
2021-06-16 17:05:58 +0530

    sidebarRouting_signupDD

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,31 +12,48 @@ export class SidebarComponent implements OnInit {
 
   @Output() isOpenSidebar = new EventEmitter();
   checkRolesPer
-  opened=false
+  opened = false
   roleName
-  constructor(private router:Router) {
-    this.checkRolesPer=JSON.parse(localStorage.getItem('userCred')) ? JSON.parse(localStorage.getItem('userCred')) : ''
+  constructor(private router: Router, private readonly oauthService: OAuthService) {
+    this.checkRolesPer = JSON.parse(localStorage.getItem('userCred')) ? JSON.parse(localStorage.getItem('userCred')) : ''
     this.roleName = localStorage.getItem('roleName') ? localStorage.getItem('roleName') : '';
-   }
+  }
 
   ngOnInit(): void {
-    console.log(this.checkRolesPer,"this.checkRolesPer")
-    console.log(this.roleName,"this.roleName")
+    console.log(this.checkRolesPer, "this.checkRolesPer")
+    console.log(this.roleName, "this.roleName")
   }
 
   isOpen = 'inActive';
 
 
-  isOpenHandle(isTrue,type){
+  isOpenHandle(isTrue, type) {
     this.isOpen = isTrue === 'inActive' ? 'active' : 'inActive';
-    this.opened = isTrue === 'inActive' ? true : false ;
-    if(type == 'open'){
-    this.isOpenSidebar.emit(this.isOpen);
+    this.opened = isTrue === 'inActive' ? true : false;
+    if (type == 'open') {
+      this.isOpenSidebar.emit(this.isOpen);
     }
+  }
+  navigatePage(path) {
+    this.opened = false
+    this.router.navigateByUrl(path)
+  }
+
+  navigateDashboardPage() {
+    let roleName = localStorage.getItem('roleName')
+    if (roleName == "icc") {
+      this.router.navigateByUrl('/icc-dashboard');
+    } else if (roleName == "financier") {
+      this.router.navigateByUrl('/financier-dashboard');
+    } else if (roleName == "sme") {
+      this.router.navigateByUrl('/sme-dashboard');
     }
-    navigatePage(path){
-      this.opened = false
-      this.router.navigateByUrl(path)
-    }
+  }
+
+  logout() {
+    this.oauthService.logOut();
+    localStorage.clear();
+    // this.authenticationService.logout()
+  }
 
 }
