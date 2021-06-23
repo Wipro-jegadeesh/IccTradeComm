94515f92850212f0e38f616e60fcd3d375d44dad
Wipro-jegadeesh <jegadeesh.baskaran@wipro.com>
2021-06-23 17:36:13 +0530

    User Idle Fixes

--- src/app/service/auth.guard.service.ts
+++ src/app/service/auth.guard.service.ts
@@ -6,6 +6,8 @@ import { environment } from 'src/environments/environment';
 import { ApiService } from './api.service';
 import { DialogDataExampleService } from '../shared/dialogBox/dialogBox.component';
 import { UserIdleService } from 'angular-user-idle';
+import {Idle, DEFAULT_INTERRUPTSOURCES} from '@ng-idle/core';
+import {Keepalive} from '@ng-idle/keepalive';
 
 @Injectable()
 export class AuthConfigService {
@@ -14,7 +16,9 @@ export class AuthConfigService {
   private _decodedIDToken: any;
   get decodedAccessToken() { return this._decodedAccessToken; }
   get decodedIDToken() { return this._decodedIDToken; }
-
+  idleState = 'Not started.';
+  timedOut = false;
+  lastPing?: Date = null;
 
   constructor(
     private readonly oauthService: OAuthService,
@@ -22,8 +26,45 @@ export class AuthConfigService {
     private router: Router,
     private apiService: ApiService,
     private dialogBox: DialogDataExampleService,
-    private userIdle: UserIdleService
-  ) { }
+    private userIdle: UserIdleService,
+    private idle: Idle, private keepalive: Keepalive
+  ) {
+    // sets an idle timeout of 5 seconds, for testing purposes.
+    idle.setIdle(5);
+    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+    idle.setTimeout(5);
+    // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+    idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+
+    idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
+    idle.onTimeout.subscribe(() => {
+      this.idleState = 'Timed out!';
+      this.timedOut = true;
+    });
+    idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
+    idle.onTimeoutWarning.subscribe((countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
+
+    // sets the ping interval to 15 seconds
+    keepalive.interval(15);
+
+    keepalive.onPing.subscribe(() => this.lastPing = new Date());
+
+    this.reset();
+   }
+   
+  ngDoCheck() {
+   
+    setTimeout(() => {
+      console.log(this.idleState, this.lastPing, 'onini', "this.idleStatecons")
+    }, 1000);
+
+
+  }
+   reset() {
+    this.idle.watch();
+    this.idleState = 'Started.';
+    this.timedOut = false;
+  }
 
   async initAuth(): Promise<any> {
     let url = window.location.pathname;
